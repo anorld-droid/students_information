cb2c593e7225ea26f8808c6487ed5cb1813dcb8b
Vincent Munene <munenevincent49@gmail.com>
2022-02-11 11:35:42 +0300

    added database functionalities

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+define("DB_SERVER","localhost");
+define("DB_USER","root");
+define("PASSWORD","");
+define('DB_NAME', 'students_informtation');
--- database.php
+++ database.php
@@ -1,2 +1,23 @@
 <?php
-echo "hello world";
+function connect($server, $user, $pass, $db_name)
+{
+    $db = new mysqli(
+        $server,
+        $user,
+        $pass
+    );
+    $sql = "CREATE DATABASE IF NOT EXISTS students_information";
+
+    if ($db->query($sql)) {
+        $db = new mysqli(
+            $server,
+            $user,
+            $pass,
+            $db_name
+        );
+
+        echo "created";
+    } else {
+        echo "Failed";
+    }
+}
